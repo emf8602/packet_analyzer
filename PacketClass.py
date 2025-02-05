7437ea09cf14540f971dd3bd71f75155416ab765
evanfp582 <evanfp582@gmail.com>
2025-02-05 10:52:15 -0500

    Overhauled filtering and arguments

--- PacketClass.py
+++ PacketClass.py
@@ -4,12 +4,14 @@ class PacketClass:
   """The class to contain a packet
   Called PacketClass because of naming conflict between scapy's Packet 
   """
-  def __init__(self, packet):
+  def __init__(self, packet, number):
     """Constructor for PacketClass
     Args:
         packet : Data from scapy read_pcap function
+        number int: The number of packet being analyzed 
     """
     self.packet = packet
+    self.number = number
     self.ether_layer = None
     self.ip_layer = None
     self.tcp_layer = None
@@ -17,19 +19,15 @@ class PacketClass:
     self.icmp_layer = None
     
     if packet.haslayer("Ether"):
-        self.ether_layer = packet["Ether"]
-
-        if packet.haslayer("IP"):
-            self.ip_layer = packet["IP"]
-
-            if packet.haslayer("TCP"):
-                self.tcp_layer = packet["TCP"]
-            
-            elif packet.haslayer("UDP"):
-                self.udp_layer = packet["UDP"]
-            
-            elif packet.haslayer("ICMP"):
-                self.icmp_layer = packet["ICMP"]
+      self.ether_layer = packet["Ether"]
+      if packet.haslayer("IP") or packet.haslayer("IPv6"):
+        self.ip_layer = packet["IP"] if packet.haslayer("IP") else packet["IPv6"]
+        if packet.haslayer("TCP"):
+          self.tcp_layer = packet["TCP"]
+        elif packet.haslayer("UDP"):
+          self.udp_layer = packet["UDP"]
+        elif packet.haslayer("ICMP"):
+          self.icmp_layer = packet["ICMP"]
 
   def parse_ethernet_layer(self):
     """Print out the ethernet layer if it exists
@@ -46,18 +44,19 @@ class PacketClass:
   def parse_ip_layer(self):
     """Print out IP layer if it exists
     """
-    if self.packet.haslayer("IP"):
+    if self.ip_layer:
       print("\nIP Layer:")
       print(f"Version: {self.ip_layer.version}")
-      print(f"Header Length: {self.ip_layer.ihl * 4} bytes ({self.ip_layer.ihl})")
-      print(f"Type of Service: {self.ip_layer.tos}")
-      print(f"Total Length: {self.ip_layer.len}")
-      print(f"Identification: {hex(self.ip_layer.id)} ({self.ip_layer.id})")
-      print(f"Flags: {flag_parse(self.ip_layer.flags)}")
-      print(f"Fragment Offset: {self.ip_layer.frag}")
-      print(f"Time to Live (TTL): {self.ip_layer.ttl}")
-      print(f"Protocol: {ProtocolNumber(self.ip_layer.proto).name} ({self.ip_layer.proto})")
-      print(f"Header Checksum: {hex(self.ip_layer.chksum)}")
+      if self.ip_layer.version == 4:
+        print(f"Header Length: {self.ip_layer.ihl * 4} bytes ({self.ip_layer.ihl})")
+        print(f"Type of Service: {self.ip_layer.tos}")
+        print(f"Total Length: {self.ip_layer.len}")
+        print(f"Identification: {hex(self.ip_layer.id)} ({self.ip_layer.id})")
+        print(f"Flags: {flag_parse(self.ip_layer.flags)}")
+        print(f"Fragment Offset: {self.ip_layer.frag}")
+        print(f"Time to Live (TTL): {self.ip_layer.ttl}")
+        print(f"Protocol: {ProtocolNumber(self.ip_layer.proto).name} ({self.ip_layer.proto})")
+        print(f"Header Checksum: {hex(self.ip_layer.chksum)}")
       print(f"Source IP Address: {self.ip_layer.src}")
       print(f"Destination IP Address: {self.ip_layer.dst}")
     else:
@@ -66,7 +65,7 @@ class PacketClass:
   def parse_tcp_layer(self):
     """Print out TCP layer if it exists
     """
-    if self.packet.haslayer("TCP"):
+    if self.tcp_layer:
       print("\nTCP Layer:")
       print(f"Source Port: {self.tcp_layer.sport}")
       print(f"Destination Port: {self.tcp_layer.dport}")
@@ -75,66 +74,62 @@ class PacketClass:
       print(f"Flags: {flag_parse(self.tcp_layer.flags)}")
       print(f"Window Size: {self.tcp_layer.window}")
       print(f"Checksum: {hex(self.tcp_layer.chksum)}")
-    # else:
-    #   print("\nNo TCP Layer")
     
   def parse_udp_layer(self):
     """Print out UDP layer if it exists"""
-    if self.packet.haslayer("UDP"):
+    if self.udp_layer:
       print("\nUDP Layer:")
       print(f"Source Port: {self.udp_layer.sport}")
       print(f"Destination Port: {self.udp_layer.dport}")
       print(f"Length: {self.udp_layer.len}")
       print(f"Checksum: {hex(self.udp_layer.chksum)}")
-    # else:
-    #   print("\nNo UPD layer")
     
   def parse_icmp_layer(self):
     """Print out ICMP layer if it exists
     """
-    if self.packet.haslayer("ICMP"):
+    if self.icmp_layer:
       print("\nICMP Layer")
       print(f"Type: {self.icmp_layer.type}")
       print(f"Code: {self.icmp_layer.code}")
       print(f"Checksum: {hex(self.icmp_layer.chksum)}")
-    # else:
-    #   print("\nNo ICMP Layer")
     
-  def valid_packet(self, packet_filter, filter_info, net):
+  def valid_packet(self, host, port, bool_filter, net):
     """Checks if the given packet is valid to be printed out
     Args:
-        packet_filter (str|None): type of filter applied
-        filter_info (str|None): extra info on filter
+        
         net (str): The source or destination IP
 
     Returns:
         bool: True if packet is valid, false if not
     """
-    if packet_filter:
-      if packet_filter == "host":
-        print("host filter")
-      elif packet_filter == "port":
-        filter_info = int(filter_info)
-        if self.tcp_layer:
-          if filter_info != self.tcp_layer.sport and filter_info != self.tcp_layer.dport:
-            return False
-        elif self.udp_layer:
-          if filter_info != self.udp_layer.sport and filter_info != self.udp_layer.dport:
-            return False
-        else:
+    if host:
+      if self.ip_layer:
+        if host != self.ip_layer.dst and host != self.ip_layer.src:
           return False
-      elif packet_filter == "ip":
-        if not self.ip_layer:
+      else:
+        return False
+    if port:
+      port = int(port)
+      if self.tcp_layer:
+        if port != self.tcp_layer.sport and port != self.tcp_layer.dport:
           return False
-      elif packet_filter == "tcp":
-        if not self.tcp_layer:
+      elif self.udp_layer:
+        if port != self.udp_layer.sport and port != self.udp_layer.dport:
           return False
-      elif packet_filter == "udp":
-        if not self.udp_layer:
-          return False
-      elif packet_filter == "icmp":
-        if not self.icmp_layer:
-          return False
+      else:
+        return False
+    if bool_filter and "ip" in bool_filter:
+      if not self.ip_layer or self.ip_layer.version != 4:
+        return False
+    if bool_filter and "tcp" in bool_filter:
+      if not self.tcp_layer:
+        return False
+    elif bool_filter and "udp" in bool_filter:
+      if not self.udp_layer:
+        return False
+    elif bool_filter and "icmp" in bool_filter:
+      if not self.icmp_layer:
+        return False
     if net:
         if net != self.packet.ip_layer.src and net != self.packet.ip_layer.dst:
             return False
@@ -142,6 +137,7 @@ class PacketClass:
   
   def print_layers(self):
     """Print all packet info layer by layer"""
+    print(f"\n----------Packet Number {self.number}----------")
     self.parse_ethernet_layer()
     self.parse_ip_layer()
     self.parse_tcp_layer()
--- pktsniffer.py
+++ pktsniffer.py
@@ -12,22 +12,30 @@ def read_pcap(filename, packet_count=None):
     """
     scapy_cap = rdpcap(filename)
     packets = []
-    for packet in scapy_cap[:packet_count]:
-        current_packet = PacketClass(packet)
+    for count, packet in enumerate(scapy_cap[:packet_count], start=1):
+        current_packet = PacketClass(packet, count)
         packets.append(current_packet)
     return packets
     
 def parse_args():
     """Parses the arguments
+    Explanation of the arguments taken from the PCAP-FILTER(7) MAN PAGES.
+        host hostnameaddr: True if either the IPv4/v6 source or destination of the packet is hostnameaddr.
+        port portnamenum: True if either the source or destination port of the packet is portnamenum.
+        ip: True if the packet is IPv4 packet.
+        tcp: True where protocol is TCP.
+        udp: True where protocol is UDP.
+        icmp: True where protocol is ICMP.
+        net netnameaddr: True if either the IPv4/v6 source or destination address of the packet has a network number of netnameaddr.
     Returns:
-        arguments
+        arguments: Namespace
     """
     parser = argparse.ArgumentParser(description="Display and Filter a Packet Sniffing File")
     
     parser.add_argument("-r", "--file", type=str, required=True, help="Read from given pcap file")
-    parser.add_argument("filter", nargs="?", choices=["host", "port", "ip", "tcp", "udp", "icmp"],
-                        help="Filters to apply on packets")
-    parser.add_argument("filter_info", nargs="?", type=str, help="Additional info for the filter criteria. Ex IP or port")
+    parser.add_argument("-host", "--host", type=str, help="Filter packet by destination")
+    parser.add_argument("-port", "--port", type=str, help="Filter packet by source port")
+    parser.add_argument("--bool_filter", nargs="*", choices=["ip", "tcp", "udp", "icmp"], help="Boolean filters to apply on packets")
     parser.add_argument("-c", "--count", type=int, help="Number of packets to capture")
     parser.add_argument("-net", "--net", type=str, help="Filter IPs that are from or going to network")
     
@@ -37,19 +45,19 @@ def main():
     """The main function to run the rest of the program."""
     args = parse_args()
     pcap_file: str = args.file
-    packet_filter: str = args.filter
-    filter_info: str = args.filter_info
+    host: str = args.host
+    port: str = args.port
+    bool_filter: str = args.bool_filter
     net: str = args.net
     packet_count: int = args.count
         
     packets = read_pcap(pcap_file, packet_count)
     filtered_packets = [
         packet for packet in packets 
-        if packet.valid_packet(packet_filter, filter_info, net)
+        if packet.valid_packet(host, port, bool_filter, net)
     ]
     
-    for count, filtered_packet in  enumerate(filtered_packets, start=1):
-        print(f"\n----------Packet Number {count}----------")
+    for filtered_packet in  filtered_packets:
         filtered_packet.print_layers()
     return
 
